63ac1d11180cc2d93178c1af5b1c45e0cf1e87c7
Naresh Kumar H <nareshkumarh@live.com>
2019-10-23 10:39:48 +0530

    Updated

---
+++ src/main/java/com/charityapp/userappms/UserappMsApplication.java
@@ -0,0 +1,13 @@
+package com.charityapp.userappms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserappMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserappMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charityapp/userappms/UserappMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charityapp.userappms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserappMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
